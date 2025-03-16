5df39e95daf5ae769f39fea1cf17e639cc1578f5
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2025-03-15 22:54:21 -0300

    feat: melhorias no controller

--- app/Http/Controllers/MetrologyCallController.php
+++ app/Http/Controllers/MetrologyCallController.php
@@ -17,93 +17,66 @@ use Illuminate\Validation\ValidationException;
 class MetrologyCallController extends Controller
 {
     public function index() {
-        $metrologyCalls = MetrologyCall::with(['machine', 'operation'])->get();
-        $machines = Machine::all();
-        $operations = Operation::all();
-
         return Inertia::render('metrology-calls', [
-            'metrologyCalls' => $metrologyCalls,
-            'machines' => $machines,
-            'operations' => $operations
+            'metrologyCalls' => MetrologyCall::with(['machine', 'operation'])->get(),
+            'machines' => Machine::all(),
+            'operations' => Operation::all()
         ]);
     }
 
     public function store(Request $request) {
-        $validator = Validator::make($request->all(), [
+        $validator = $this->validateRequest($request);
+
+        if ($validator->fails()) {
+            $errors = $this->customizeValidationErrors($validator);
+            throw ValidationException::withMessages($errors);
+        }
+
+        $request['status'] = 'waiting_receive';
+        MetrologyCall::create($request->all());
+        
+        return redirect()->route('metrology-calls.index');
+    }
+
+    public function update($id, Request $request) {
+        $validator = $this->validateRequest($request);
+
+       if ($validator->fails()) {
+            $errors = $this->customizeValidationErrors($validator);
+            throw ValidationException::withMessages($errors);
+        }
+
+        $metrologyCall = MetrologyCall::findOrFail($id);
+        $metrologyCall->update($request->all());
+
+        return redirect()->route('metrology-calls.index');
+    }
+
+    public function destroy($id) {
+        $metrologyCall = MetrologyCall::findOrFail($id);
+        $metrologyCall->delete();
+
+        return redirect()->route('metrology-calls.index');
+    }
+
+    public function export() {
+        return Excel::download(new MetrologyCallExport, 'chamados.xlsx');
+    }
+
+    protected function validateRequest(Request $request) {
+        return Validator::make($request->all(), [
           'item_name' => 'required|string|max:255',
           'machine_id' => 'required|exists:machines,id',
           'operation_id' => 'required|exists:operations,id',
           'type' => 'required|in:setup,production,adjust'
         ]);
-
-        if ($validator->fails()) {
-            $errors = $validator->errors()->toArray();
-            foreach ($errors as $field => $messages) {
-                $errors[$field] = 'Este campo é obrigatório.';
-            }
-            throw ValidationException::withMessages($errors);
-        }
-
-        try {
-            $request['status'] = 'waiting_receive';
-            MetrologyCall::create($request->all());
-            
-            return redirect()->route('metrology-calls.index');
-        } catch (Exception $e) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Erro ao criar o chamado de metrologia.',
-                'error' => $e->getMessage(),
-            ], 500);
-        }
     }
 
-    public function update($id, Request $request) {
-        $validator = Validator::make($request->all(), [
-            'item_name' => 'required|string|max:255',
-            'machine_id' => 'required|exists:machines,id',
-            'operation_id' => 'required|exists:operations,id',
-            'type' => 'required|in:setup,production,adjust'
-        ]);
-
-        if ($validator->fails()) {
-            $errors = $validator->errors()->toArray();
-            foreach ($errors as $field => $messages) {
-                $errors[$field] = 'Este campo é obrigatório.';
-            }
-            throw ValidationException::withMessages($errors);
+    protected function customizeValidationErrors($validator) {
+        $errors = $validator->errors()->toArray();
+        foreach ($errors as $field => $messages) {
+            $errors[$field] = 'Este campo é obrigatório.';
         }
-
-        try{
-            $metrologyCall = MetrologyCall::find($id);
-            $metrologyCall->update($request->all());
-            
-            return redirect()->route('metrology-calls.index');
-        }catch(Exception $e) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Erro ao atualizar o chamado de metrologia.',
-                'error' => $e->getMessage(),
-            ], 500);
-        }
-    }
-
-    public function destroy($id) {
-        try{
-            $metrologyCall = MetrologyCall::find($id);
-            $metrologyCall->delete();
-            
-            return redirect()->route('metrology-calls.index');
-        }catch(Exception $e) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Erro ao deletar o chamado de metrologia.',
-                'error' => $e->getMessage(),
-            ], 500);
-        }
-    }
-
-    public function export() {
-        return Excel::download(new MetrologyCallExport, 'chamados.xlsx');
+        return $errors;
     }
 }
