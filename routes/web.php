d2351fa479c1485dd522a4b31a3560a649b687a2
Lucas André Beniti Bernardo <lucasbeniti.bernardo@gmail.com>
2025-06-22 19:05:31 -0300

    feat: adicionando middleware para as rotas de admin

--- routes/web.php
+++ routes/web.php
@@ -8,60 +8,63 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\OperationController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\CheckUserRole;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::controller(DashboardController::class)->prefix('/dashboard')->name('dashboard.')->group(function() {
-        Route::get('/', 'index')->name('index');
-    });
+    Route::middleware(CheckUserRole::class)->group(function () {
+        Route::controller(DashboardController::class)->prefix('/dashboard')->name('dashboard.')->group(function() {
+            Route::get('/', 'index')->name('index');
+        });
 
-    Route::controller(MachineController::class)->prefix('/machines')->name('machines.')->group(function() {
-        Route::get('/', 'index')->name('index');
-        Route::post('/', 'store')->name('store');
-        Route::put('/{id}', 'update')->name('update');
-        Route::delete('/{id}', 'destroy')->name('destroy');
-        Route::get('/export', 'export')->name('export');
-    });
-
-    Route::controller(ClientController::class)->prefix('/clients')->name('clients.')->group(function() {
-        Route::get('/', 'index')->name('index');
-        Route::post('/', 'store')->name('store');
-        Route::put('/{id}', 'update')->name('update');
-        Route::delete('/{id}', 'destroy')->name('destroy');
-        Route::get('/export', 'export')->name('export');
-    });
-
-    Route::controller(CostCenterController::class)->prefix('/cost-centers')->name('cost-centers.')->group(function() {
-        Route::get('/', 'index')->name('index');
-        Route::post('/', 'store')->name('store');
-        Route::put('/{id}', 'update')->name('update');
-        Route::delete('/{id}', 'destroy')->name('destroy');
-        Route::get('/export', 'export')->name('export');
-    });
-
-    Route::controller(ItemController::class)->prefix('/items')->name('items.')->group(function() {
-        Route::get('/', 'index')->name('index');
-        Route::post('/', 'store')->name('store');
-        Route::put('/{id}', 'update')->name('update');
-        Route::delete('/{id}', 'destroy')->name('destroy');
-        Route::get('/export', 'export')->name('export');
-
-        // Subgrupo para operações do item selecionado
-        Route::controller(OperationController::class)->prefix('/{item}/operations')->name('operations.')->group(function () {
+        Route::controller(MachineController::class)->prefix('/machines')->name('machines.')->group(function() {
             Route::get('/', 'index')->name('index');
             Route::post('/', 'store')->name('store');
-            Route::put('/{operation}', 'update')->name('update'); 
-            Route::delete('/{operation}', 'destroy')->name('destroy');
+            Route::put('/{id}', 'update')->name('update');
+            Route::delete('/{id}', 'destroy')->name('destroy');
             Route::get('/export', 'export')->name('export');
         });
-    });
 
-    Route::controller(UserController::class)->prefix('/users')->name('users.')->group(function() {
-        Route::get('/', 'index')->name('index');
-        Route::post('/', 'store')->name('store');
-        Route::put('/{id}', 'update')->name('update');
-        Route::delete('/{id}', 'destroy')->name('destroy');
-        Route::get('/export', 'export')->name('export');
+        Route::controller(ClientController::class)->prefix('/clients')->name('clients.')->group(function() {
+            Route::get('/', 'index')->name('index');
+            Route::post('/', 'store')->name('store');
+            Route::put('/{id}', 'update')->name('update');
+            Route::delete('/{id}', 'destroy')->name('destroy');
+            Route::get('/export', 'export')->name('export');
+        });
+
+        Route::controller(CostCenterController::class)->prefix('/cost-centers')->name('cost-centers.')->group(function() {
+            Route::get('/', 'index')->name('index');
+            Route::post('/', 'store')->name('store');
+            Route::put('/{id}', 'update')->name('update');
+            Route::delete('/{id}', 'destroy')->name('destroy');
+            Route::get('/export', 'export')->name('export');
+        });
+
+        Route::controller(ItemController::class)->prefix('/items')->name('items.')->group(function() {
+            Route::get('/', 'index')->name('index');
+            Route::post('/', 'store')->name('store');
+            Route::put('/{id}', 'update')->name('update');
+            Route::delete('/{id}', 'destroy')->name('destroy');
+            Route::get('/export', 'export')->name('export');
+
+            // Subgrupo para operações do item selecionado
+            Route::controller(OperationController::class)->prefix('/{item}/operations')->name('operations.')->group(function () {
+                Route::get('/', 'index')->name('index');
+                Route::post('/', 'store')->name('store');
+                Route::put('/{operation}', 'update')->name('update'); 
+                Route::delete('/{operation}', 'destroy')->name('destroy');
+                Route::get('/export', 'export')->name('export');
+            });
+        });
+
+        Route::controller(UserController::class)->prefix('/users')->name('users.')->group(function() {
+            Route::get('/', 'index')->name('index');
+            Route::post('/', 'store')->name('store');
+            Route::put('/{id}', 'update')->name('update');
+            Route::delete('/{id}', 'destroy')->name('destroy');
+            Route::get('/export', 'export')->name('export');
+        });
     });
 
     Route::controller(MetrologyCallController::class)->prefix('/metrology-calls')->name('metrology-calls.')->group(function() {
