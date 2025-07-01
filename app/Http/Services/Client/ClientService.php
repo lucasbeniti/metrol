70ccb7307e018d3e87964121251a5857164512c7
Lucas André Beniti Bernardo <lucasbeniti.bernardo@gmail.com>
2025-07-01 20:28:35 -0300

    feat: adicionando logs no crud de clientes

--- app/Http/Services/Client/ClientService.php
+++ app/Http/Services/Client/ClientService.php
@@ -2,19 +2,25 @@
 
 namespace App\Http\Services\Client;
 
+use App\Enums\LogActionsEnum;
+use App\Enums\LogTablesEnum;
 use App\Http\Repositories\Client\ClientRepositoryInterface;
+use App\Http\Services\Log\LogServiceInterface;
 use App\Models\Client;
 use Exception;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class ClientService implements ClientServiceInterface
 {
     protected ClientRepositoryInterface $clientRepository;
+    protected LogServiceInterface $logService;
 
-    public function __construct(ClientRepositoryInterface $clientRepository)
+    public function __construct(ClientRepositoryInterface $clientRepository, LogServiceInterface $logService)
     {
         $this->clientRepository = $clientRepository;
+        $this->logService = $logService;
     }
 
     public function getAll(): Collection
@@ -37,7 +43,18 @@ class ClientService implements ClientServiceInterface
 
         $data['password'] = Hash::make('123');
 
-        return $this->clientRepository->store($data);
+        $client = $this->clientRepository->store($data);
+
+        $authenticatedUser = Auth::user();
+
+        $this->logService->store([
+            'user_id' => $authenticatedUser->id,
+            'action_id' => LogActionsEnum::CREATE,
+            'description' => 'O usuário ' . $authenticatedUser->name . ' criou o cliente: ' . $client->name,
+            'table_id' => LogTablesEnum::CLIENTS,
+        ]);
+
+        return $client;
     }
 
     public function update(int $id, array $data): bool
@@ -48,7 +65,20 @@ class ClientService implements ClientServiceInterface
             throw new Exception('Já existe um cliente com esse código.');
         }
 
-        return $this->clientRepository->update($id, $data);
+        $success = $this->clientRepository->update($id, $data);
+
+        if ($success) {
+            $authenticatedUser = Auth::user();
+
+            $this->logService->store([
+                'user_id' => $authenticatedUser->id,
+                'action_id' => LogActionsEnum::UPDATE,
+                'description' => 'O usuário ' . $authenticatedUser->name . ' atualizou o cliente: ' . $data['name'],
+                'table_id' => LogTablesEnum::CLIENTS,
+            ]);
+        }
+
+        return $success;
     }
 
     public function destroy(int $id): bool
@@ -59,6 +89,19 @@ class ClientService implements ClientServiceInterface
             throw new Exception('Não é possível excluir um cliente que possui centros de custo associados.');
         }
 
-        return $this->clientRepository->destroy($id);
+        $success = $this->clientRepository->destroy($id);
+
+        if ($success) {
+            $authenticatedUser = Auth::user();
+
+            $this->logService->store([
+                'user_id' => $authenticatedUser->id,
+                'action_id' => LogActionsEnum::DELETE,
+                'description' => 'O usuário ' . $authenticatedUser->name . ' deletou o cliente: ' . $client->name,
+                'table_id' => LogTablesEnum::CLIENTS,
+            ]);
+        }
+
+        return $success;
     }
 }
