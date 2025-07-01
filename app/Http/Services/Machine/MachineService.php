b0b44b7f7b097f0877683532b2ca008c3f8a0954
Lucas André Beniti Bernardo <lucasbeniti.bernardo@gmail.com>
2025-07-01 20:53:22 -0300

    feat: adicionando logs no crud de máquinas

--- app/Http/Services/Machine/MachineService.php
+++ app/Http/Services/Machine/MachineService.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Services\Machine;
 
+use App\Enums\LogActionsEnum;
+use App\Enums\LogTablesEnum;
 use App\Http\Repositories\Machine\MachineRepositoryInterface;
+use App\Http\Services\Log\LogServiceInterface;
 use App\Models\Machine;
 use Exception;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
 
 class MachineService implements MachineServiceInterface
 {
     protected MachineRepositoryInterface $machineRepository;
+    protected LogServiceInterface $logService;
 
-    public function __construct(MachineRepositoryInterface $machineRepository)
+    public function __construct(MachineRepositoryInterface $machineRepository, LogServiceInterface $logService)
     {
         $this->machineRepository = $machineRepository;
+        $this->logService = $logService;
     }
 
     public function getAll(): Collection
@@ -34,7 +40,18 @@ class MachineService implements MachineServiceInterface
             throw new Exception('Já existe uma máquina com esse código.');
         }
 
-        return $this->machineRepository->store($data);
+        $machine = $this->machineRepository->store($data);
+
+        $authenticatedUser = Auth::user();
+
+        $this->logService->store([
+            'user_id' => $authenticatedUser->id,
+            'action_id' => LogActionsEnum::CREATE,
+            'description' => 'O usuário ' . $authenticatedUser->name . ' criou a máquina: ' . $machine->name,
+            'table_id' => LogTablesEnum::OPERATIONS,
+        ]);
+
+        return $machine;
     }
 
     public function update(int $id, array $data): bool
@@ -45,7 +62,20 @@ class MachineService implements MachineServiceInterface
             throw new Exception('Já existe uma máquina com esse código.');
         }
 
-        return $this->machineRepository->update($id, $data);
+        $success = $this->machineRepository->update($id, $data);
+
+        if ($success) {
+            $authenticatedUser = Auth::user();
+
+            $this->logService->store([
+                'user_id' => $authenticatedUser->id,
+                'action_id' => LogActionsEnum::UPDATE,
+                'description' => 'O usuário ' . $authenticatedUser->name . ' atualizou a máquina: ' . $data['name'],
+                'table_id' => LogTablesEnum::OPERATIONS,
+            ]);
+        }
+
+        return $success;
     }
 
     public function destroy(int $id): bool
@@ -56,6 +86,19 @@ class MachineService implements MachineServiceInterface
             throw new Exception('Não é possível excluir uma máquina que possui chamados de metrologia associadas.');
         }
 
-        return $this->machineRepository->destroy($id);
+        $success = $this->machineRepository->destroy($id);
+
+        if ($success) {
+            $authenticatedUser = Auth::user();
+
+            $this->logService->store([
+                'user_id' => $authenticatedUser->id,
+                'action_id' => LogActionsEnum::DELETE,
+                'description' => 'O usuário ' . $authenticatedUser->name . ' deletou a máquina: ' . $machine->name,
+                'table_id' => LogTablesEnum::OPERATIONS,
+            ]);
+        }
+
+        return $success;
     }
 }
